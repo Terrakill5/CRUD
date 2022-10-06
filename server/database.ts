edf762ae0983900442a7144f9a1344bb8f436c4e
JBriceno <josealbertino4@gmail.com>
2022-10-06 07:16:55 -0500

    Agregado servidor mongo atlas

--- server/database.ts
+++ server/database.ts
@@ -2,8 +2,8 @@ import { connect } from "mongoose";
 
 export const startConnection = async () => {
   try {
-    const db = await connect("mongodb://localhost/mevn-database");
-    console.log(db.connection.name);
+    /* const db = await connect("mongodb://localhost/mevn-database");TerraNeil  7tbW7Q1NMA4w1fMn*/
+    await connect("mongodb+srv://TerraNeil:7tbW7Q1NMA4w1fMn@crud3.ppzpwd0.mongodb.net/?retryWrites=true&w=majority");
   } catch (error)
   {
     console.error(error);
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import {startConnection} from './database';
-
+ 
 startConnection();
 
 app.listen(3000);
